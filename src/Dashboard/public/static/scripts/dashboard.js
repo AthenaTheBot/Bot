72e510ddb6f6e92e0069f834ecca5dc48dc97f5e
Tuna <tunasayinn@gmail.com>
2021-05-19 15:22:31 +0300

    Fixed some issues related to dashboard

--- src/Dashboard/public/static/scripts/dashboard.js
+++ src/Dashboard/public/static/scripts/dashboard.js
@@ -17,29 +17,58 @@ const selectLang = (language, id) => {
     $('.languages').css('display', 'none');
     menuOpened = false;
 
+    let display;
     switch(language) {
         case 'en-US':
-            language = 'English';
+            display = 'English';
             break;
         case 'tr-TR':
-            language = 'Türkçe';
+            display = 'Türkçe';
             break;
         case 'defaultLang':
-            language = null;
+            display = null;
             break;
         default:
-            language = null;
+            display = null;
             break;
     }
 
-    if (!language) return;
+    if (!display) return;
 
-    $('#languageButton').text(language);
+    $('#languageButton').text(display);
 
-    console.log(language);
+    fetch('/dashboard/actions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ operation: 'setLanguage',  guildID: id, language: language }),
+    })
+    .then(response => response.json())
+    .then(data => {
+
+        if (data.status == 200) {
+
+            addWarning('Successfully made changes!');
+            return;
+        }
+        else {
+
+            addWarning('An unexpected error occured while trying to make changes!');
+            return;
+        }
+    })
+    .catch(error => {
+
+        addWarning('An unexpected error occured while trying to make changes!');
+        return;
+    });
 
     return;
 }
+
+let warnCount = 0;
+let warnRemoved = 0;
 
 const setPrefix = (event, id) => {
 
@@ -64,14 +93,21 @@ const setPrefix = (event, id) => {
             .then(response => response.json())
             .then(data => {
 
-                if (data.status == 200) return console.log('Operation successfull!');
+                if (data.status == 200) {
+
+                    addWarning('Successfully made changes!');
+                    return;
+                }
                 else {
 
-                    return console.error('An error occured!');
+                    addWarning('An unexpected error occured while trying to make changes!');
+                    return;
                 }
             })
-            .catch((error) => {
-                  console.error('Error:', error);
+            .catch(error => {
+
+                addWarning('An unexpected error occured while trying to make changes!');
+                return
             });
         }
     }
@@ -80,3 +116,31 @@ const setPrefix = (event, id) => {
         return;
     };
 };
+
+const addWarning = (text) => {
+
+    $('.warnings').append(`
+    <p id="warn_` + warnCount + `" class="warn">${text}</p>
+    `)
+
+    $(`#warn_${warnCount}`).fadeIn();
+
+    warnCount = warnCount + 1;
+
+    setTimeout(() => {
+        
+        $(`#warn_${warnRemoved}`).fadeOut();
+
+        setTimeout(() => {
+            $(`#warn_${warnRemoved}`).remove();
+        }, 1000);
+
+        warnRemoved = warnRemoved + 1;
+
+        if (warnRemoved == warnCount) {
+            warnCount = 0;
+            warnRemoved = 0;
+        }
+
+    }, 5000);
+}
--- src/Dashboard/routers/dashboard.js
+++ src/Dashboard/routers/dashboard.js
@@ -57,6 +57,34 @@ router.get('/:id', async (req, res) => {
 
     if (!selectedGuildData) return res.redirect('/dashboard');
     else {
+
+        const resultDB = await Athena.db.manager.getGuild(selectedGuildData);
+
+        if (resultDB && resultDB.data &&  resultDB.data.preferences) {
+            switch(resultDB.data.preferences.language) {
+                case 'en-US':
+                    selectedGuild.language = 'English';
+                    break;
+                case 'tr-TR':
+                    selectedGuild.language = 'Türkçe';
+                    break;
+                default:
+                    selectedGuild.language = null;
+                    break;
+            }
+
+            if (resultDB.data.preferences.prefix) {
+                selectedGuild.prefix = resultDB.data.preferences.prefix;
+            }
+            else {
+
+                selectedGuild.prefix = null;
+            }
+        }
+        else {
+            selectedGuild.language = null;
+        }
+
         selectedGuild.memberCount = selectedGuildData.memberCount;
         selectedGuild.channelCount = selectedGuildData.channels.cache.size;
         selectedGuild.dashURL = '/dashboard/' + selectedGuildData.id;
@@ -77,41 +105,44 @@ router.post('/actions', async (req, res) => {
 
     if (req.ip == '::1') {
 
-        switch(req.body.operation) {
-            case 'setPrefix':
-                if (req.body.value) return res.status(400).json({ status: 400, message: 'Bad Request' }).end();
-                try {
-
-                    await Athena.db.manager.setValue({ collection: 'servers', query: { _id: req.body.guildID }, operation: { $set: { "data.preferences.prefix": req.body.prefix } } });
-                }
-                catch(err) {
-
-                    Athena.handleError({ error: err, print: true });
-                    return res.status(500).json({ status: 500, message: 'Server Error' }).end();
-                }
-                return res.status(200).json({ status: 200, message: 'Successfull' }).end();
-                break;
-            case 'setLanguage':
-                if (req.body.value) return res.status(400).json({ status: 400, message: 'Bad Request' }).end();
-                try {
-
+        if (req.method == 'POST') {
+        
+            switch(req.body.operation) {
+                case 'setPrefix':
+                    if (!req.body.prefix) return res.status(400).json({ status: 400, message: 'Bad Request' }).end();
+                    try {
+    
+                        await Athena.db.manager.setValue({ collection: 'servers', query: { _id: req.body.guildID }, operation: { $set: { "data.preferences.prefix": req.body.prefix } } });
+                    }
+                    catch(err) {
+    
+                        Athena.handleError({ error: err, print: true });
+                        return res.status(500).json({ status: 500, message: 'Server Error' }).end();
+                    }
+                    res.status(200).json({ status: 200, message: 'Successfull' }).end();
+                    break;
+                case 'setLanguage':
+                    if (!req.body.language) return res.status(400).json({ status: 400, message: 'Bad Request' }).end();
                     const validLangauges = ['en-US', 'tr-TR'];
-
                     if (!validLangauges.includes(req.body.language)) return res.status(400).json({ status: 400, message: 'Bad Request' }).end();
+                    try {
+        
+                        await Athena.db.manager.setValue({ collection: 'servers', query: { _id: req.body.guildID }, operation: { $set: { "data.preferences.language": req.body.language } } });
+                    }
+                    catch(err) {
+    
+                        Athena.handleError({ error: err, print: true });
+                        return res.status(500).json({ status: 500, message: 'Server Error' }).end();
+                    }
+                    res.status(200).json({ status: 200, message: 'Successfull' }).end();
+                    break;
+                default:
+                    res.status(400).json({ status: 400, message: 'Bad Request' }).end();
+                    break;
+            }
 
-                    await Athena.db.manager.setValue({ collection: 'servers', query: { _id: req.body.guildID }, operation: { $set: { "data.preferences.language": req.body.language } } });
-                }
-                catch(err) {
-
-                    Athena.handleError({ error: err, print: true });
-                    return res.status(500).json({ status: 500, message: 'Server Error' }).end();
-                }
-                return res.status(200).json({ status: 200, message: 'Successfull' }).end();
-                break;
-            default:
-                res.status(400).json({ status: 400, message: 'Bad Request' }).end();
-                break;
-        }
+            return;
+        };
     };
 
 });
