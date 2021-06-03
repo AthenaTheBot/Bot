ace82d618b748e090f4192e27f2ca0fcf1a40cb0
Tuna <tunasayinn@gmail.com>
2021-06-03 18:32:30 +0300

    Added main comp

--- src/Dashboard/public/assets/scripts/dashboard.js
+++ src/Dashboard/public/assets/scripts/dashboard.js
@@ -6,11 +6,13 @@ $(document).ready(() => {
         if (menuExpanded) {
 
             $('#profileDropdown').css('display', 'none');
+            $('#profileButton').css('transform', 'rotate(0deg)');
             menuExpanded = false;
         }
         else {
 
             $('#profileDropdown').css('display', 'block');
+            $('#profileButton').css('transform', 'rotate(180deg)');
             menuExpanded = true;
         }
     }) 
