e065b4e9664771f35ff4ca986cd48b601cb83a85
Tuna <tunasayinn@gmail.com>
2021-10-19 17:36:24 +0300

    Fixed bot not sending any message while playing a song.

--- src/Commands/Configuration/language.js
+++ src/Commands/Configuration/language.js
@@ -1,60 +1,94 @@
-const BaseCommand = require('../../Structures/Command');
+const BaseCommand = require("../../Structures/Command");
 
-const { MessageEmbed } = require('discord.js');
-const { readdirSync } = require('fs');
-const { join } = require('path');
+const { MessageEmbed } = require("discord.js");
+const { readdirSync } = require("fs");
+const { join } = require("path");
 
 class Command extends BaseCommand {
-    constructor(){
-        super({
-            name: 'language',
-            aliases: [],
-            description: 'Command for setting language.',
-            category: 'Configuration',
-            usage: '[language]',
-            options: [{
-                type: 'STRING',
-                name: 'language',
-                description: 'Language that you want to change.',
-                required: false
-            }],
-            cooldown: 4,
-            required_perms: [],
-            required_bot_perms: ["SEND_MESSAGES", "EMBED_LINKS", "READ_MESSAGE_HISTORY"]
-        });
+  constructor() {
+    super({
+      name: "language",
+      aliases: [],
+      description: "Command for setting language.",
+      category: "Configuration",
+      usage: "[language]",
+      options: [
+        {
+          type: "STRING",
+          name: "language",
+          description: "Language that you want to change.",
+          required: false,
+        },
+      ],
+      cooldown: 4,
+      required_perms: [],
+      required_bot_perms: [
+        "SEND_MESSAGES",
+        "EMBED_LINKS",
+        "READ_MESSAGE_HISTORY",
+      ],
+    });
+  }
+
+  async run(client, msg, args, locale) {
+    const Embed = new MessageEmbed().setColor("#5865F2");
+    const guildData = await client.dbManager.getGuild(msg.guild.id);
+
+    const availableLanguages = readdirSync(
+      join(__dirname, "..", "..", "Locales")
+    );
+
+    if (!args[0])
+      return msg.reply({
+        embeds: [
+          Embed.setDescription(
+            locale.LANGUAGE_INFO.replace(
+              "$language",
+              guildData.data.preferences.language
+            )
+          ),
+        ],
+      });
+    else if (args[0] && !availableLanguages.includes(args[0])) {
+      return msg.reply({
+        embeds: [
+          Embed.setDescription(
+            locale.INVALID_LANGUAGE.replace(
+              "$languages",
+              "[`" + availableLanguages.join("`, `") + "`]"
+            )
+          ),
+        ],
+      });
     }
 
-    async run(client, msg, args, locale){
+    try {
+      await client.dbManager.updateDocument("guild", msg.guild.id, {
+        $set: { "data.preferences.language": args[0].trim() },
+      });
 
-        const Embed = new MessageEmbed().setColor('#5865F2');
-        const guildData = await client.dbManager.getGuild(msg.guild.id);
-
-        const availableLanguages = readdirSync(join(__dirname, '..', '..', 'Locales'));
-
-        if (!args[0]) return msg.reply({ embeds: [Embed.setDescription(locale.LANGUAGE_INFO.replace('$language', guildData.data.preferences.language))] });
-        else if (args[0] && !availableLanguages.includes(args[0])) {
-
-            return msg.reply({ embeds: [Embed.setDescription(locale.INVALID_LANGUAGE.replace('$languages', '[`' + availableLanguages.join('`, `') + '`]'))] });
-        }
-
-        try {
-
-            await client.dbManager.updateDocument('guild', msg.guild.id, { $set: { "data.preferences.language": args[0].trim() } });
-        }
-        catch(err){
-
-            client.log(2, err);
-            return msg.reply({ embeds: [
-                Embed.setDescription(locale.ERROR)
-            ] });
-        };
-
-        locale = client.locales.get(args[0].trim()).locales.find(lang => lang.cmd == 'language').content;
-
-        return msg.reply({ embeds: [
-            Embed.setDescription(locale.SUCCESS.replace('$language', args[0].trim()))
-        ] });
+      // Update play command locale
+      const playCmdLocale = client.locales
+        .get(args[0])
+        .locales.find((x) => x.cmd == "play").content;
+      client.songStates.get(msg.guild.id).locale = playCmdLocale;
+    } catch (err) {
+      client.log(2, err);
+      return msg.reply({ embeds: [Embed.setDescription(locale.ERROR)] });
     }
+
+    locale = client.locales
+      .get(args[0].trim())
+      .locales.find((lang) => lang.cmd == "language").content;
+
+    return msg.reply({
+      embeds: [
+        Embed.setDescription(
+          locale.SUCCESS.replace("$language", args[0].trim())
+        ),
+      ],
+    });
+  }
 }
 
 module.exports = Command;
--- src/Commands/Music/play.js
+++ src/Commands/Music/play.js
@@ -1,244 +1,284 @@
-const BaseCommand = require('../../Structures/Command');
+const BaseCommand = require("../../Structures/Command");
 const {
-    createAudioPlayer,
-    createAudioResource,
-    joinVoiceChannel,
-    VoiceConnectionStatus,
-    AudioPlayerStatus,
-    StreamType,
-    getVoiceConnection
-
-} = require('@discordjs/voice');
-const { MessageEmbed } = require('discord.js');
-const playDL = require('play-dl');
-const spotify = require('spotify-url-info');
-const Track = require('../../Structures/Track');
+  createAudioPlayer,
+  createAudioResource,
+  joinVoiceChannel,
+  VoiceConnectionStatus,
+  AudioPlayerStatus,
+  StreamType,
+  getVoiceConnection,
+} = require("@discordjs/voice");
+const { MessageEmbed } = require("discord.js");
+const playDL = require("play-dl");
+const spotify = require("spotify-url-info");
+const Track = require("../../Structures/Track");
 
 class Command extends BaseCommand {
-    constructor(){
-        super({
-            name: 'play',
-            aliases: ["p"],
-            description: 'Command for playing song from youtube.',
-            category: 'Music',
-            usage: '[song]',
-            options: [{
-                type: 'STRING',
-                name: 'song',
-                description: 'Song name or link (youtube).',
-                required: true
-            }],
-            cooldown: 4,
-            required_perms: [],
-            required_bot_perms: ["SEND_MESSAGES", "READ_MESSAGE_HISTORY", "CONNECT", "SPEAK"]
+  constructor() {
+    super({
+      name: "play",
+      aliases: ["p"],
+      description: "Command for playing song from youtube.",
+      category: "Music",
+      usage: "[song]",
+      options: [
+        {
+          type: "STRING",
+          name: "song",
+          description: "Song name or link (youtube).",
+          required: true,
+        },
+      ],
+      cooldown: 4,
+      required_perms: [],
+      required_bot_perms: [
+        "SEND_MESSAGES",
+        "READ_MESSAGE_HISTORY",
+        "CONNECT",
+        "SPEAK",
+      ],
+    });
+  }
+
+  async run(client, msg, args, locale) {
+    let guildState = client.songStates.get(msg.guild.id);
+    const requestedSong = args.join(" ");
+    const Embed = new MessageEmbed().setColor("#5865F2");
+    const defaultGuild = {
+      playing: false,
+      queue: [],
+      guild: msg.guild,
+      voiceChannel: msg.member.voice.channel,
+      player: null,
+      locale: locale,
+    };
+
+    if (!requestedSong)
+      return msg.reply({ embeds: [Embed.setDescription(locale.INVALID_SONG)] });
+
+    if (!guildState) {
+      client.songStates.set(msg.guild.id, defaultGuild);
+      guildState = defaultGuild;
+    }
+
+    if (guildState.playing) {
+      const queueSong = await this.getSong(requestedSong);
+      if (!queueSong)
+        return msg.reply({
+          embeds: [Embed.setDescription(locale.SONG_NOT_FOUND)],
         });
-    }
-
-    async run(client, msg, args, locale) {
-
-        let guildState = client.songStates.get(msg.guild.id);
-        const requestedSong = args.join(' ');
-        const Embed = new MessageEmbed().setColor('#5865F2');
-        const defaultGuild = {
-            playing: false,
-            queue: [],
-            guild: msg.guild,
-            voiceChannel: msg.member.voice.channel,
-            player: null
-        }
-
-        if (!requestedSong) return msg.reply({ embeds: [ Embed.setDescription(locale.INVALID_SONG) ] });
-
-        if (!guildState) {
-            client.songStates.set(msg.guild.id, defaultGuild)
-            guildState = defaultGuild;
-        }
-
-        if (guildState.playing) {
-            const queueSong = await this.getSong(requestedSong);
-            if (!queueSong) return msg.reply({ embeds: [ Embed.setDescription(locale.SONG_NOT_FOUND) ] });
-            guildState.queue.push(queueSong);
-            let song = null;
-            if (queueSong.length && queueSong.length > 0) {
-                queueSong.forEach(item => {
-                    guildState.queue.push(item);
-                })
-    
-                song = queueSong[0];
-            }
-            return msg.reply({ embeds: [ Embed.setDescription(locale.SONG_ADDED_TO_QUEUE.replace('$song', `[${queueSong.title}](${queueSong.url})`)) ] });
-        };
-
-        let song = await this.getSong(requestedSong);
-
-        if (!song) return msg.reply({ embeds: [ Embed.setDescription(locale.SONG_NOT_FOUND) ] });
-        else if (song.length && song.length > 0) {
-            song.forEach(item => {
-                guildState.queue.push(item);
-            })
-
-            song = song[0];
-        }
-
-        guildState.queue.push(song);
-        
-        this.playSong(client, msg, guildState);
-    }
-
-    // Aditional Functions
-    async getSong(reqSong) {
-        const type = playDL.validate(reqSong);
-        if (!type) {
-            const ytSearchResult = await playDL.search(reqSong, { limit: 1, type: 'video' });
-            if (ytSearchResult[0]) {
-
-                return new Track(ytSearchResult[0].url, ytSearchResult[0].title, ytSearchResult[0].channel.name, ytSearchResult[0].durationInSec);
-            }
-            else {
-
-                return null;
-            }
-        }
-        else if (type.startsWith('sp')) {
-            if (type == 'sp_track') {
-                const spotifyTrackData = await spotify.getData(reqSong);
-                if (spotifyTrackData) {
-                    const artistNames = [];
-                    spotifyTrackData.artists.forEach(artist => artistNames.push(artist.name));
-                    const ytSearchResult = await playDL.search(`${artistNames.join(' ')} ${spotifyTrackData.name}`, { limit: 1, type: 'video' });
-                    if (ytSearchResult[0]) {
-
-                        return new Track(ytSearchResult[0].url, ytSearchResult[0].title, ytSearchResult[0].channel.name, ytSearchResult[0].durationInSec);
-                    }
-                    else {
-
-                        return null;
-                    }
-                }
-                else {
-
-                    return null;
-                }
-            }
-            else {
-
-
-            }
-        }
-        else if (type.startsWith('yt')) {
-            if (type == 'yt_video') {
-                const videoInfo = await playDL.video_basic_info(reqSong);
-                return new Track(videoInfo.video_details.url, videoInfo.video_details.title, videoInfo.video_details.channel.name, videoInfo.video_details.url);
-            }
-            else {
-                const playlistInfo = await playDL.playlist_info(reqSong, true);
-                const videos = [];
-                if (playlistInfo?.videos) {
-                    playlistInfo.videos.forEach(video => {
-                        videos.push(new Track(video.url, video.title, playlistInfo.channel.name, video.durationInSec));
-                    })
-                }
-
-                if (videos.length > 0) return videos;
-                else return null;
-            }
-        }
-    }
-
-    async playSong(client, msg, guildState, skipCommand) {
-        async function startedPlaying(playing, guildState) {
-            const Embed = new MessageEmbed();
-            if (!playing) msg.reply({ embeds: [ Embed.setDescription(locale.ERROR) ] });
-            else {
-
-                if (guildState.queue[0].msgSent) return;
-
-                try {
-
-                    await msg.reply({ embeds: [ Embed.setDescription(locale.NOW_PLAYING.replace('$song', `[${guildState.queue[0].title}](${guildState.queue[0].url})`)) ] });
-
-                }
-                catch(err) {
-
-                    return;
-                }
-                
-                guildState.queue[0].msgSent = true;
-            }
-        }
-
-        if (guildState.playing) return;
-
-        const player = createAudioPlayer();
-        const source = await playDL.stream(guildState.queue[0].url);
-        const resource = createAudioResource(source.stream, 
-        { inputType: source.type });
-
-        if (!guildState.player) guildState.player = player;
-
-        player.play(resource)
-
-        if (skipCommand) startedPlaying(true, guildState);
-
-        player.on('error', (err) => {
-            console.log('ERROR:', err);
-            startedPlaying(false);
-            subscription.unsubscribe();
-            guildState.playing = false;
-            player.stop();
-            const vc = getVoiceConnection(guildState.guild.id);
-            vc.destroy();
-        })
-
-        const connection = await joinVoiceChannel({ 
-            channelId: guildState.voiceChannel.id,
-            guildId: guildState.guild.id, 
-            selfDeaf: true, 
-            selfMute: false, 
-            adapterCreator: guildState.guild.voiceAdapterCreator 
+      guildState.queue.push(queueSong);
+      let song = null;
+      if (queueSong.length && queueSong.length > 0) {
+        queueSong.forEach((item) => {
+          guildState.queue.push(item);
         });
 
-        const subscription = connection.subscribe(player);
-
-        player.on('stateChange', (oldState, newState) => {
-            if (newState.status == AudioPlayerStatus.Playing) { 
-                if (startedPlaying) startedPlaying(true, guildState);
-                guildState.playing = true;
-            }
-
-            if (newState.status == AudioPlayerStatus.Idle) {
-                guildState.playing = false;
-                guildState.queue.shift();
-
-                if (guildState.queue.length > 0) {
-                    this.playSong(client, msg, guildState);
-                    if (startedPlaying) startedPlaying(true, guildState);
-                }
-                else {
-                    setTimeout(() => {
-                        if (guildState.queue.length == 0) {
-                            guildState.playing = false;
-                            player.stop();
-                            const vc = getVoiceConnection(guildState.guild.id);
-                            vc?.destroy();
-                            client.songStates.delete(guildState.guild.id);
-                        }
-                    }, 2 * 60 * 1000)
-                }
-            }
-        })
-
-        connection.on('stateChange', (oldState, newState) => {
-            if (newState.status == VoiceConnectionStatus.Disconnected) {
-                guildState.playing = false;
-                setTimeout(() => {
-                    const newGuildState = client.songStates.get(guildState.guild.id);
-                    if (!newGuildState.playing) client.songStates.delete(guildState.guild.id);
-                }, 5 * 60 * 1000)
-            }
-        })
-
+        song = queueSong[0];
+      }
+      return msg.reply({
+        embeds: [
+          Embed.setDescription(
+            locale.SONG_ADDED_TO_QUEUE.replace(
+              "$song",
+              `[${queueSong.title}](${queueSong.url})`
+            )
+          ),
+        ],
+      });
     }
+
+    let song = await this.getSong(requestedSong);
+
+    if (!song)
+      return msg.reply({
+        embeds: [Embed.setDescription(locale.SONG_NOT_FOUND)],
+      });
+    else if (song.length && song.length > 0) {
+      song.forEach((item) => {
+        guildState.queue.push(item);
+      });
+
+      song = song[0];
+    }
+
+    guildState.queue.push(song);
+
+    this.playSong(client, msg, guildState);
+  }
+
+  // Aditional Functions
+  async getSong(reqSong) {
+    const type = playDL.validate(reqSong);
+    if (!type) {
+      const ytSearchResult = await playDL.search(reqSong, {
+        limit: 1,
+        type: "video",
+      });
+      if (ytSearchResult[0]) {
+        return new Track(
+          ytSearchResult[0].url,
+          ytSearchResult[0].title,
+          ytSearchResult[0].channel.name,
+          ytSearchResult[0].durationInSec
+        );
+      } else {
+        return null;
+      }
+    } else if (type.startsWith("sp")) {
+      if (type == "sp_track") {
+        const spotifyTrackData = await spotify.getData(reqSong);
+        if (spotifyTrackData) {
+          const artistNames = [];
+          spotifyTrackData.artists.forEach((artist) =>
+            artistNames.push(artist.name)
+          );
+          const ytSearchResult = await playDL.search(
+            `${artistNames.join(" ")} ${spotifyTrackData.name}`,
+            { limit: 1, type: "video" }
+          );
+          if (ytSearchResult[0]) {
+            return new Track(
+              ytSearchResult[0].url,
+              ytSearchResult[0].title,
+              ytSearchResult[0].channel.name,
+              ytSearchResult[0].durationInSec
+            );
+          } else {
+            return null;
+          }
+        } else {
+          return null;
+        }
+      } else {
+      }
+    } else if (type.startsWith("yt")) {
+      if (type == "yt_video") {
+        const videoInfo = await playDL.video_basic_info(reqSong);
+        return new Track(
+          videoInfo.video_details.url,
+          videoInfo.video_details.title,
+          videoInfo.video_details.channel.name,
+          videoInfo.video_details.url
+        );
+      } else {
+        const playlistInfo = await playDL.playlist_info(reqSong, true);
+        const videos = [];
+        if (playlistInfo?.videos) {
+          playlistInfo.videos.forEach((video) => {
+            videos.push(
+              new Track(
+                video.url,
+                video.title,
+                playlistInfo.channel.name,
+                video.durationInSec
+              )
+            );
+          });
+        }
+
+        if (videos.length > 0) return videos;
+        else return null;
+      }
+    }
+  }
+
+  async playSong(client, msg, guildState, skipCommand) {
+    async function startedPlaying(playing, guildState) {
+      const Embed = new MessageEmbed();
+      if (!playing) msg.reply({ embeds: [Embed.setDescription(locale.ERROR)] });
+      else {
+        if (guildState.queue[0].msgSent) return;
+
+        await msg
+          .reply({
+            embeds: [
+              Embed.setColor("#5865F2").setDescription(
+                guildState.locale.NOW_PLAYING.replace(
+                  "$song",
+                  `[${guildState.queue[0].title}](${guildState.queue[0].url})`
+                )
+              ),
+            ],
+          })
+          .catch((err) => {});
+
+        guildState.queue[0].msgSent = true;
+      }
+    }
+
+    if (guildState.playing) return;
+
+    const player = createAudioPlayer();
+    const source = await playDL.stream(guildState.queue[0].url);
+    const resource = createAudioResource(source.stream, {
+      inputType: source.type,
+    });
+
+    if (!guildState.player) guildState.player = player;
+
+    player.play(resource);
+
+    if (skipCommand) startedPlaying(true, guildState);
+
+    player.on("error", (err) => {
+      console.log("ERROR:", err);
+      startedPlaying(false);
+      subscription.unsubscribe();
+      guildState.playing = false;
+      player.stop();
+      const vc = getVoiceConnection(guildState.guild.id);
+      vc.destroy();
+    });
+
+    const connection = await joinVoiceChannel({
+      channelId: guildState.voiceChannel.id,
+      guildId: guildState.guild.id,
+      selfDeaf: true,
+      selfMute: false,
+      adapterCreator: guildState.guild.voiceAdapterCreator,
+    });
+
+    const subscription = connection.subscribe(player);
+
+    player.on("stateChange", (oldState, newState) => {
+      if (newState.status == AudioPlayerStatus.Playing) {
+        if (startedPlaying) startedPlaying(true, guildState);
+        guildState.playing = true;
+      }
+
+      if (newState.status == AudioPlayerStatus.Idle) {
+        guildState.playing = false;
+        guildState.queue.shift();
+
+        if (guildState.queue.length > 0) {
+          this.playSong(client, msg, guildState);
+          if (startedPlaying) startedPlaying(true, guildState);
+        } else {
+          setTimeout(() => {
+            if (guildState.queue.length == 0) {
+              guildState.playing = false;
+              player.stop();
+              const vc = getVoiceConnection(guildState.guild.id);
+              vc?.destroy();
+              client.songStates.delete(guildState.guild.id);
+            }
+          }, 2 * 60 * 1000);
+        }
+      }
+    });
+
+    connection.on("stateChange", (oldState, newState) => {
+      if (newState.status == VoiceConnectionStatus.Disconnected) {
+        guildState.playing = false;
+        setTimeout(() => {
+          const newGuildState = client.songStates.get(guildState.guild.id);
+          if (!newGuildState.playing)
+            client.songStates.delete(guildState.guild.id);
+        }, 5 * 60 * 1000);
+      }
+    });
+  }
 }
 
 module.exports = Command;
